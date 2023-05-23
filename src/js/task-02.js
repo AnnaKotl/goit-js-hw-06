4858cfe8fcdce825f9ea510b2ad8355ebcdf4cfa
Анна Котляр <kotlyaranya1771@gmail.com>
2023-05-23 20:40:23 +0300

    task 2 start

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const listOfIngredients = document.querySelector('#ingredients');
+
+// const makeIngredientsItems = ingredient => {
+//   return ingredient.map(ingredient => {
+//     ingredients.forEach(ingredient => {
+//       const item = document.createElement('li');
+//       item.textContent = ingredient;
+//       item.classList.add('item');
+//       listOfIngredients.appendChild(item);
+//     });
+//   });
+// };
+// document.body.appendChild(listOfIngredients);
