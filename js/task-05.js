d01fd028dac10c882062ff0e7c7eab90aba80215
Анна Котляр <kotlyaranya1771@gmail.com>
2023-06-04 14:14:41 +0300

    4, 5, 6 done

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+const inputValue = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+inputValue.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    if (inputValue.value.trim() !== "") {
+    outputName.textContent = event.currentTarget.value;
+    } else {
+    outputName.textContent = "Anonymous";
+    }
+}
+
