f3372b4bc58a190b9f97fc5e690f434bdea09ec0
Анна Котляр <kotlyaranya1771@gmail.com>
2023-06-04 15:55:40 +0300

    7, 8,, 9 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
+// console.log(form);
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email= formElements.email.value;
+    const password = formElements.password.value;
+
+    if (!email || !password) {
+        alert('Всі поля повинні бути заповнені!');
+        return;
+    }
+
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData);
+
+    form.reset();
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const colorText = document.querySelector('.color');
+const changeBtn = document.querySelector('.change-color');
+const bodyStyle = document.body;
+
+changeBtn.addEventListener('click', onBtnChangeColor);
+
+function onBtnChangeColor() {
+  bodyStyle.style.backgroundColor = getRandomHexColor();
+  colorText.textContent = bodyStyle.style.backgroundColor;
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
